178f1583998ffa0171f051dc18b8e7d372fa272d
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-08-20 00:10:41 -0700

    Unfinished lights out project created

---
+++ lights-out/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Board from "./Board";
+import Cell from "./Cell";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Board />
+        <Cell />
+      </div>
+    );
+  }
+}
+
+export default App;
